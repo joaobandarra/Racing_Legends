0fa27423482d5a2db2b83df9ad1e93eedfa256d0
joaobandarra <joaogsbandarra@gmail.com>
2018-04-12 14:27:40 +0100

    Feito o exercicio 0 Do projeto de TI 2 - Racing Legends -  Da componente do professor André Carvalho

---
+++ Racing_Legends/Racing_Legends/JavaScript/Logic.js
@@ -0,0 +1,26 @@
+﻿document.addEventListener('DOMContentLoaded', function main() {
+    var xhr = new XMLHttpRequest();
+    /**
+     * @type {Element}
+     */
+    var racingLegends = null;
+
+    xhr.open('GET', '/Racing_Legends/Index.html');
+
+    xhr.onload = function (_) {
+        if (xhr.status === 200) {
+            racingLegends = xhr.responseXML;
+            init();
+
+        } else {
+            console.error('Erro', xhr.status);
+        }
+    };
+
+    xhr.onerror = function (_) {
+        console.error('Erro');
+    };
+
+    xhr.send();
+
+    function init() { }
